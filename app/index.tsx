c2b58d13a67e2cca347539e021088f1eeced73c4
Boris Migashko <tuxoh992@gmail.com>
2024-11-12 15:54:01 +0100

    Add lint, prettier, tsc, rm default screens

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { View, Text } from 'react-native';
+
+const Root: FC = () => {
+  return (
+    <View>
+      <Text>Root</Text>
+    </View>
+  );
+};
+
+export default Root;
